33c9378c5677b48cadd24e79388582c6d33d630b
JumanjiJob <35mirzhan@mail.ru>
2024-07-30 10:31:43 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))   # Задаем размеры экрана
+
+pygame.display.set_caption("Игра Тир")                            # Название окна
+icon = pygame.image.load("")
+
+
 running = True
 while running:
     pass
